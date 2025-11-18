532b62abb1cc65c6514512af3ad57ebe5c032dc1
Valentin Yuryev <goodfaiter@gmail.com>
2025-11-18 15:00:03 +0000

    vllm fixes

--- src/gen_ea_rl/models/vllm.py
+++ src/gen_ea_rl/models/vllm.py
@@ -5,7 +5,7 @@ from typing import Optional
 import os
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 os.environ['OPENAI_API_KEY'] = 'token-abc123'
 os.environ['MISTRAL_API_KEY'] = 'token-abc123'
 
@@ -21,7 +21,7 @@ class vllm:
             mode = Mode.JSON_SCHEMA
         else:
             mode = Mode.TOOLS
-        self.client = patch(OpenAI(base_url="http://localhost:8000/v1"), mode=mode)
+        self.client = patch(OpenAI(base_url="http://localhost:8000/v1"), mode=mode, max_retries=5)
         self.model_name = model_name
 
     def __call__(self, **kwargs):
@@ -29,7 +29,7 @@ class vllm:
         prompt = kwargs.get('prompt', None)
 
         messages = []
-        if mode.developer():
+        if hasattr(mode, "developer"):
             messages.append({"role": "developer", "content": mode.developer()})
         messages.append({"role": "user", "content": prompt})
 
@@ -39,6 +39,7 @@ class vllm:
             reasoning_effort=self.reasoning_effort,
             temperature=self.temperature,
             top_p=self.top_p,
+            logprobs= True if self.top_logprobs is not None else False,
             top_logprobs=self.top_logprobs,
             messages=messages,
         )
